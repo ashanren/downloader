503b0590d18442e8c92d66f9bd65355240360da5
ashanren <jcp38@njit.edu>
2019-05-01 17:18:47 -0400

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+
+import * as fastify from "fastify";
+//import routes from "app/routes";
+import { log } from "config/logging";
+//const port    = process.env.PORT || require('config/config').port;
+import { port } from "config/config";
+
+const start = async () => {
+  try {
+    log.warn("PORT: ", port);
+    const app: fastify.FastifyInstance = fastify();
+    //routes(app);
+    app.listen(port, '0.0.0.0');
+    log.info(`Download Server Started - Listening on port: ${port}`);
+  } catch (err) {
+    log.error("Failed to Start Download Server:", err);
+  }
+}
+
+start();
+
